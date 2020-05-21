453cfdc85733caa04c719d3f7562ec4ac10fc321
Lord-Stanly <Fizikello@mail.ru>
2020-05-21 10:35:19 +0300

    3.8 готов и заполнен экран about

--- app/src/main/java/ru/mail/fizikello/sudoku/MainActivity.java
+++ app/src/main/java/ru/mail/fizikello/sudoku/MainActivity.java
@@ -2,13 +2,39 @@ package ru.mail.fizikello.sudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Обработчик нажатий для всех кнопок
+        View continueButton = findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(this);
+
+        View newButton = findViewById(R.id.new_button);
+        newButton.setOnClickListener(this);
+
+        View aboutButton = findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+
+        View exitButton = findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
+
+    }
+
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.about_button:
+                Intent i = new Intent(this, About.class);
+                startActivity(i);
+                break;
+        }
     }
 }
